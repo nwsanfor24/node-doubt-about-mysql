8e5411498d7912bbc9502e3bd1ab880408b60171
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-11 19:13:13 -0400

    made rock music

---
+++ rockMusic.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "root",
+    database: "playlistDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+})
