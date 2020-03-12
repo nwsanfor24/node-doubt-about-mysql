398725e2f33dcd127d37909681adf2960ded2c64
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-11 20:01:45 -0400

    it works - swamp rock!

--- rockMusic.js
+++ rockMusic.js
@@ -11,16 +11,45 @@ var connection = mysql.createConnection({
     database: "playlistDB"
 });
 
+function updateGenre() {
+    console.log("Updating all SWAMP ROCK genres...\n");
+    var query = connection.query(
+        "UPDATE songs SET ? WHERE ?",
+        [
+            {
+                genre: "Swamp Rock"
+            },
+            {
+                genre: "Classic Rock"
+            }
+        ],
+    )
+}
+
+function createSong() {
+    console.log("Inserting a new song... \n");
+    var query = connection.query(
+        "INSERT INTO songs SET ?",
+        {
+            title: "Heavy/Like A Witch",
+            artist: "All Them Witches",
+            genre: "Swamp Rock"
+        },
+        function(err, res) {
+            if (err) throw err;
+            console.log(res.affectedRows + " song inserted!\n");
+
+            updateGenre();
+        }
+    );
+
+    console.log(query.sql)
+}
+
+
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
+    createSong();
 });
-
-function afterConnection() {
-    connection.query('SELECT * FROM songs', function(err, res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
-    });
-}
