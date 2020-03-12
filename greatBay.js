fee5e4cd6c513c4ace63ae620341b20b3a3cf74f
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-11 21:00:20 -0400

    made many a change

--- greatBay.js
+++ greatBay.js
@@ -1,4 +1,5 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,9 +15,11 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    console.table(res);
 
     Promise.all([
-        createItems()
+        createItems(),
+        readItems()
     ]).then( () => {
         console.log("All done!");
         connection.end();
@@ -40,4 +43,84 @@ function createItems() {
 
 function logRow({ id, item, price, quantity }) {
     console.log(`${id} | ${item} | ${price} | ${quantity}`);
+}
+
+function readItems() {
+    connection.query('SELECT * FROM items', function (err, res) {
+        if (err) throw err;
+        console.log("----------ITEMS----------")
+        console.table(res);
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: 'What do you want?',
+                choices: ["POST AN ITEM", "BID ON AN ITEM"],
+                name: "action"
+            }
+        ]).then(answers => {
+            let action = answers.action;
+            console.log(action);
+
+            if (action = "POST AN ITEM") {
+                addItem();
+            } else (action = "BID ON AN ITEM") {
+                bidItem();
+            }
+
+            function bidItem() {
+                console.log("--------------BID ON AN ITEM------------");
+                readItems();
+            }
+
+            function addItem() {
+                console.log("-----------------ADD ITEMS-----------------");
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: "Item Name?",
+                        name: "itemName"
+                    },
+                    {
+                        type: 'input',
+                        message: "Item Price?",
+                        name: "itemPrice"
+                    },
+                    {
+                        type: 'input',
+                        message: "Quantity?",
+                        name: "itemQuantity"
+                    }
+                ]).then(answers => {
+                    let itemName = answers.itemName;
+                    let itemPrice = answers.itemPrice;
+                    let itemQuantity = answers.itemQuantity;
+                    let query = connection.query(
+                        "INSERT INTO ITEMS SET ?",
+                        {
+                            item: itemName,
+                            price: itemPrice,
+                            quantity: itemQuantity
+                        },
+                        function(err, res) {
+                            if (err) throw err;
+                            console.log(`-------${res.affectedRows} ITEMS INSERTED----------`);
+                            console.log(`ITEM: ${itemName} PRICE: ${itemPrice} QUANTITY: ${itemQuantity}`);
+
+                            readItems();
+                        }
+                    );
+                });
+
+            function readItems() {
+                console.log("-----------ITEMS-------------");
+                connection.query("SELECT * FROM items", function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    connection.end();
+                });
+            }
+            }
+        });
+    });
 }
